ab80ab73ad09938a8eccab4c14b6f88d554e08e4
vicSystems <victorasuquob@gmail.com>
2022-09-11 12:02:44 +0100

    receipt upoad

--- app/Http/Controllers/PaymentScheduleController.php
+++ app/Http/Controllers/PaymentScheduleController.php
@@ -79,8 +79,8 @@ class PaymentScheduleController extends Controller
             try {
                 //code...
 
-                Mail::to('verify@leptonsecovest.com')
-                ->send(new PaymentReceiptMail($datax));
+                // Mail::to('verify@leptonsecovest.com')
+                // ->send(new PaymentReceiptMail($datax));
 
 
                 Mail::to($user_payment->plan->user->email)
